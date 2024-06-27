e03b20006ffe2d2bcdaf2a45f0285e4fe5ef79b5
tinkugupta5 <tinku.gupta4@gmail.com>
2024-06-27 19:39:50 +0530

    Revert "Merge pull request #1 from arpanam11/main"
    This reverts commit c8c79a8f1443bf44e11720692d2a62e36fe388b2, reversing
    changes made to 7ab632a882fcadfcade9d6019e0ac9f9b82f9c69.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Loaders</h1>
-    <p>The React Loaders Collection</p>
+      <h1>Hellow World</h1>
     </div>
   );
 }
